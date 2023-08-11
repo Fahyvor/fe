1ad8b39bb164388ee928e72953a28b512ca9834d
Fahyvor <favourokafor30@gmail.com>
2023-08-11 11:30:57 +0100

    Contact Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Dashboard from './Components/Dashboard/Dashboard'
-import ContactComponent from './Components/Contact/Contact'
+import ContactComponent from './Components/Contact/ContactComponent'
 
 function App() {
   const [count, setCount] = useState(0)
