78cc4205c0702b4e44086fbf877eff9938b74ee2
Fahyvor <favourokafor30@gmail.com>
2023-08-11 22:06:17 +0100

    New changes

--- src/Components/Contact/ContactComponent.tsx
+++ src/Components/Contact/ContactComponent.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 
+const MaxInputFields = 5;
+
 interface Address {
   street: string;
   city: string;
   state: string;
 }
+
 
 interface Contact {
   name: string;
@@ -21,6 +24,26 @@ const ContactComponent: React.FC = () => {
   const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
   const [contacts, setContacts] = useState<Contact[]>([]);
+  const [inputFields, setInputFields] = useState<string[]>(['']);
+
+  const AddField = () => {
+    if (inputFields.length < MaxInputFields) {
+      setInputFields([...inputFields, '']);
+    }
+  };
+
+  const handleInputChange = (index: number, value: string) => {
+    const newInputFields = [...inputFields];
+    newInputFields[index] = value;
+    setInputFields(newInputFields);
+  };
+
+  const handleRemoveField = (index: number) => {
+    if (inputFields.length > 1) {
+      const newInputFields = inputFields.filter((_, i) => i !== index);
+      setInputFields(newInputFields);
+    }
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -40,24 +63,49 @@ const ContactComponent: React.FC = () => {
   };
 
   return (
-    <div className='w-screen bg-white color-black
-    text-black h-screen flex flex-col justify-center items-center'>
-      <h2 className='text-center text-4xl font-bold'>Add Contact</h2>
-      <form onSubmit={handleSubmit} className='flex flex-col w-1/3 mt-5 gap-4'>
+    <div className='w-full bg-white max-sm:w-full color-black h-screen
+    text-black flex flex-col justify-center px-5'>
+      <h2 className='text-4xl font-bold'>Add Contact</h2>
+      <form onSubmit={handleSubmit} className='grid grid-cols-2 w-2/3 max-sm:w-full gap-4'>
+        <div className='flex flex-col'>
         <label>Name:</label>
         <input type="text"
         className="p-3 mt-2 bg-slate-200" value={name} onChange={(e) => setName(e.target.value)} required />
+        </div>
+        <div className='flex flex-col'>
         <label>Phone Number:</label>
         <input type="text" 
         className="p-3 mt-2 bg-slate-200" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} required  />
+        </div>
+        <div className='flex flex-col'>
         <label>Email:</label>
         <input type="email" 
         className="p-3 mt-2 bg-slate-200" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </div>
+        <div className='flex flex-col'>
         <label>Address:</label>
-        <input type="text" 
-        className="p-3 mt-2 bg-slate-200" value={address} onChange={(e) => setAddress(e.target.value)} required />
+        {/* <input type="text" 
+        className="p-3 mt-2 bg-slate-200" value={address} onChange={(e) => setAddress(e.target.value)} required /> */}
+
+        {inputFields.map((value, index) => (
+          <div key={index} className='w-full'>
+            <input
+              type="text"
+              value={value}
+              className="p-3 mt-2 bg-slate-200"
+              onChange={(e) => handleInputChange(index, e.target.value)}
+            />
+          </div>
+        ))}
+        </div>
+        <div className='flex flex-col'>
         <button type="submit"
-        className='mt-5 bg-black text-white'>Add Contact</button>
+        className='mt-5 bg-blue-900 rounded-none text-white'>Submit</button>
+        </div>
+
+        <div onClick={AddField}
+        className='mt-5 bg-blue-900 rounded-none 
+        text-center flex items-center justify-center text-white'>Add Contact</div>
       </form>
     </div>
   );
--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ContactComponent from '../Contact/ContactComponent';
 
 const Dashboard: React.FC = () => {
   const [contacts, setContacts] = useState<Contact[]>([]);
@@ -9,51 +10,115 @@ const Dashboard: React.FC = () => {
   const [address, setAddress] = useState('');
   const [longitude, setLongitude] = useState(0);
   const [latitude, setLatitude] = useState(0);
+  const [dashboard, setDashboard] = useState(true);
+  const [contact, setContact] = useState(false);
+
+  const showDashboard = () => {
+    setDashboard(true);
+    setContact(false);
+  }
+
+  const showContact = () => {
+    setContact(true);
+    setDashboard(false);
+  }
 
 
   return (
-    <div className='flex gap-5 w-screen pt-5 bg-white color-black text-black
+    <div className='flex w-screen bg-white color-black text-black
     h-screen'>
-      <div className='flex flex-col'>
-        <h2 className='text-center text-3xl font-bold'>Dashboard</h2>
-        <div className='add-contact flex justify-end px-5'>
-          <Link to='/contact' className='bg-black text-white
-          rounded-full p-3'>Add Contact</Link>
+      <div className='menu bg-blue-900 pt-6 w-1/5 text-white px-3 
+      max-sm:abolute max-sm:inset-y-0'>
+        <h3 className='text-3xl pb-4'>Logo</h3>
+
+        <div className='dashboard mt-7' onClick={showDashboard}>
+          <h2>Dashboard</h2>
         </div>
-        <table >
-          <thead>
-            <tr className='flex w-screen mt-5 gap-1 justify-between
-            max-sm:gap-6 max-sm:flex-col px-8'>
-              <th className='bg-slate-100 p-2 w-1/6
-              max-sm:w-1/3'>Name</th>
-              <th className='bg-slate-100 p-2 w-1/6
-              max-sm:w-1/3'>Phone Number</th>
-              <th className='bg-slate-100 p-2 w-1/6
-              max-sm:w-1/3'>Email</th>
-              <th className='bg-slate-100 p-2 w-1/6
-              max-sm:w-1/3'>Address</th>
-              <th className='bg-slate-100 p-2 w-1/6
-              max-sm:w-1/3'>Longitude</th>
-              <th className='bg-slate-100 p-2 w-1/6
-              max-sm:w-1/3'>Latitude</th>
-            </tr>
-          </thead>
-          <tbody>
-            {contacts.map((contact, index) => (
-              <tr key={index}>
-                <td>{contact.name}</td>
-                <td>{contact.phoneNumber}</td>
-                <td>{contact.email}</td>
-                <td>{contact.addresses[0]?.street}</td>
-                <td>{contact.longitude}</td>
-                <td>{contact.latitude}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className='add-contacts mt-5' onClick={showContact}>
+          <h2>Add Contacts</h2>
+        </div>
       </div>
+      
+      { dashboard ? (<div className='menu-display pt-6  w-5/6 px-5 flex flex-col gap-5'>
+        <div className='dashboard-display flex flex-col gap-3'>
+          <h2 className='text-3xl'>Dashboard</h2>
+          <p>Welcome back, User</p>
+
+          <small>You do not have any user on your contact list</small>
+        </div>
+
+        <button className='bg-blue-900 text-white
+        w-2/4 rounded-none' onClick={showContact}>Add Contact</button>
+      </div>):
+      null }
+
+        {contact ? ( 
+          <ContactComponent />
+        ): null
+
+        }
     </div>
   );
 };
 
 export default Dashboard;
+
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+// const Dashboard: React.FC = () => {
+//   const [contacts, setContacts] = useState<Contact[]>([]);
+//   const [name, setName] = useState('');
+//   const [phoneNumber, setPhoneNumber] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [address, setAddress] = useState('');
+//   const [longitude, setLongitude] = useState(0);
+//   const [latitude, setLatitude] = useState(0);
+
+
+//   return (
+//     <div className='flex gap-5 w-screen pt-5 bg-white color-black text-black
+//     h-screen'>
+//       <div className='flex flex-col'>
+//         <h2 className='text-center text-3xl font-bold'>Dashboard</h2>
+//         <div className='add-contact flex justify-end px-5'>
+//           <Link to='/contact' className='bg-black text-white
+//           rounded-full p-3'>Add Contact</Link>
+//         </div>
+//         <table >
+//           <thead>
+//             <tr className='flex w-screen mt-5 gap-1 justify-between
+//             max-sm:gap-6 max-sm:flex-col px-8'>
+//               <th className='bg-slate-100 p-2 w-1/6
+//               max-sm:w-1/3'>Name</th>
+//               <th className='bg-slate-100 p-2 w-1/6
+//               max-sm:w-1/3'>Phone Number</th>
+//               <th className='bg-slate-100 p-2 w-1/6
+//               max-sm:w-1/3'>Email</th>
+//               <th className='bg-slate-100 p-2 w-1/6
+//               max-sm:w-1/3'>Address</th>
+//               <th className='bg-slate-100 p-2 w-1/6
+//               max-sm:w-1/3'>Longitude</th>
+//               <th className='bg-slate-100 p-2 w-1/6
+//               max-sm:w-1/3'>Latitude</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {contacts.map((contact, index) => (
+//               <tr key={index}>
+//                 <td>{contact.name}</td>
+//                 <td>{contact.phoneNumber}</td>
+//                 <td>{contact.email}</td>
+//                 <td>{contact.addresses[0]?.street}</td>
+//                 <td>{contact.longitude}</td>
+//                 <td>{contact.latitude}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Dashboard;
